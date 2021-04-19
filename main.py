5fe4df40351d9dc22c5d4adfb05173df398b7bba
Jordan <jordanlevy96@gmail.com>
2021-04-19 13:23:25 +0100

    check /r/all; remove all caps case (apparently it's a medical term or something)

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import re
 from collections import deque
 
 reddit = praw.Reddit('DEFAULT', config_interpolation='basic')
-subreddit = reddit.subreddit('AskReddit')
+subreddit = reddit.subreddit('all')
 
-ect_regex = '\Wect\W'
+ect_regex = '\W[Ee]ct\W'
 cache = deque(maxlen=200) # maintain a cache to avoid duplicating effort
 
 running = True
@@ -16,9 +16,11 @@ while running:
          break
       cache.append(comment.id)
 
-      if re.search(ect_regex, comment.body, re.IGNORECASE):
+      if re.search(ect_regex, comment.body):
          try:
             print(comment.body)
+            print(comment.permalink)
+            print('---------------------')
             # do stuff
 
          except Exception as e:
